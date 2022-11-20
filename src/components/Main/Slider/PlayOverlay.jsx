55aff3ba1a9de74a796932db6cb04086f6b4782b
TravikDev <travikdev@gmail.com>
2022-11-20 23:37:10 +0000

    Youtube fix

--- src/components/Main/Slider/PlayOverlay.jsx
+++ src/components/Main/Slider/PlayOverlay.jsx
@@ -9,7 +9,7 @@ function PlayOverlay({ onClose = (f) => f }) {
         className="fixed z-20 h-full w-full bg-black opacity-30"
       />
 
-      <div className="fixed z-30 my-[172px] mx-[5px] h-auto md:h-[505px] min-w-[350px] bg-white p-[15px] md:mx-auto md:w-[70%] md:max-w-[1310px] md:p-[30px]">
+      <div className="fixed z-30 my-[172px] mx-[5px] h-auto md:h-[617px] min-w-[350px] bg-white p-[15px] md:mx-auto md:w-[70%] md:max-w-[1310px] md:p-[30px]">
         <div className="mb-[15px] flex items-center justify-between md:mb-[20px]">
           <span className="mt-0 text-center text-[20px] font-bold md:text-[26px]">
             Natural & Cultured Stones
@@ -30,7 +30,7 @@ function PlayOverlay({ onClose = (f) => f }) {
           width="1250"
           height={505}
         /> */}
-        <iframe width="100%" height="87%" src="https://www.youtube.com/embed/utOU0sNkRjY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe width="100%" height="89%" src="https://www.youtube.com/embed/utOU0sNkRjY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
     </>
   );
